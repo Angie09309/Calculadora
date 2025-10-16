ffa0113ffd34510356be61f6a1675b9971dc9baa
Angie Barreto <angiepbg13@gmail.com>
2025-10-16 15:52:21 -0500

    Simplificar la lógica de renderizado condicional en el componente Boton

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -3,19 +3,28 @@ import "../style/boton.css";
 
 const Boton = (props) => {
   const esOperador = (valor) => {
-    return isNaN(valor) && valor != "." && valor != "=";
+    return isNaN(valor) && valor !== "." && valor !== "=";
   };
 
-  return (
-    <div
-      className={`boton-contenedor ${
-        esOperador(props.children) ? "operador" : ""
-      }`.trimEnd()}
-      onClick={() => props.manejarClic(props.children)}
-    >
-      {props.children}
-    </div>
-  );
+  if (esOperador(props.children)) {
+    return (
+      <div
+        className="boton-contenedor operador"
+        onClick={() => props.manejarClic(props.children)}
+      >
+        {props.children}
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className="boton-contenedor"
+        onClick={() => props.manejarClic(props.children)}
+      >
+        {props.children}
+      </div>
+    );
+  }
 };
 
 export default Boton;
